077e6993362c521083b803deaf502a46e619f202
Sarkisyan Veronica <Combo-Breaker@users.noreply.github.com>
2020-02-17 23:15:15 +0300

    add imports

---
+++ sem_01/ imports.py
@@ -0,0 +1,38 @@
+# 1. Intro.ipynb
+import yargy
+import nltk
+import rusenttokenize 
+import re
+from pymystem3 import Mystem
+from pymorphy2 import MorphAnalyzer
+
+# 1. Regex in Python.ipynb
+import re
+
+# 1. Keywords and Topic Modelling.ipynb
+import numpy 
+import pandas 
+import re
+import sklearn
+import random
+import matplotlib
+import seaborn
+import gensim
+import pyLDAvis
+import tqdm
+import bokeh
+import seaborn
+import scipy
+
+
+#1. Embeddings.ipynb
+import gensim
+import logging
+import nltk
+import nltk.data 
+import pandas
+import urllib.request
+import bs4 
+import vecto
+import bpemb
+import pyemd
